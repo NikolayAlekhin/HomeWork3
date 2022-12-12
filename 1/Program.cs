d02ff7bc372827a3bb930033a930bf99f4d27729
Nikolay Alekhin <kolyan.alekhin@mail.ru>
2022-12-12 12:59:01 +0300

    pers1

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*   Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.WriteLine("Введите  число");
+string namberOnerStr = Console.ReadLine() ??"";
+
+if (namberOnerStr[0] == namberOnerStr[4]&& (namberOnerStr[1] == namberOnerStr[3]))
+{
+ Console.WriteLine( $"{namberOnerStr} = да");
+}
+else
+{
+     Console.WriteLine( $"{namberOnerStr} = нет");
+}
